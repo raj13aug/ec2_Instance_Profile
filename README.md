4c038ad5e04eb4308ce5ad3e6c88d5fa2666b539
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-10-29 08:49:37 +0530

    update role

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 iam-tutorial
+
+creating an AWS IAM role using terraform.
+creating an IAM policy using terraform.
+attaching the policy to the role using terraform.
+creating the IAM instance profile using terraform.
+Assigning the IAM role, to an EC2 instance on the fly using terraform.
--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+# Declaring the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_iam_role" "role" {
   name = "test_role"
   path = "/"
@@ -9,7 +14,7 @@ resource "aws_iam_role" "role" {
         {
             "Action": "sts:AssumeRole",
             "Principal": {
-               "Service": "ec2.amazonaws.com"
+               "Service": "ec2.amazonaws.com", "s3.amazonaws.com"
             },
             "Effect": "Allow",
             "Sid": ""
@@ -24,6 +29,42 @@ resource "aws_iam_instance_profile" "test_profile" {
   role = aws_iam_role.role.name
 }
 
+
+resource "aws_iam_policy" "policy" {
+  name        = "iam-policy-for-instance-role"
+  description = "iam-policy-for-instance-role"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*","s3:Get*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-s3-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -37,7 +78,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["932999788441"] # Canonical
+  owners = ["932999788441"]
 }
 
 resource "aws_instance" "web" {
@@ -46,6 +87,6 @@ resource "aws_instance" "web" {
   iam_instance_profile = aws_iam_instance_profile.test_profile.id
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Instance_profile"
   }
 }
